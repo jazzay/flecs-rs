8d55e53a1a1e2917a0ace14f026fe4deec91c280
Jason Jarvis <jazzay@gmail.com>
2022-12-06 17:48:20 -0800

    update bench to bevy 0.9.1 and add Flax ECS

--- benches/ecs_compare.rs
+++ benches/ecs_compare.rs
@@ -1,38 +1,38 @@
 use criterion::*;
 
-use bevy_ecs::prelude::Component;
-
 const ITER_COUNT: usize = 100_000;
-
-#[derive(Component, Copy, Clone, Default, Debug, PartialEq)]
-struct Position {
-	x: f32,
-	y: f32,
-	z: f32,
-}
-
-#[derive(Component, Copy, Clone, Default, Debug, PartialEq)]
-struct Rotation {
-	x: f32,
-	y: f32,
-	z: f32,
-}
-
-#[derive(Component, Copy, Clone, Default, Debug, PartialEq)]
-struct Velocity {
-	x: f32,
-	y: f32,
-	z: f32,
-}
 
 mod bevy_bench {
 	use super::*;
 	use bevy_ecs::prelude::*;
 
+	use bevy_ecs::prelude::Component;
+
+	#[derive(Component, Copy, Clone, Default, Debug, PartialEq)]
+	struct Position {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
+
+	#[derive(Component, Copy, Clone, Default, Debug, PartialEq)]
+	struct Rotation {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
+
+	#[derive(Component, Copy, Clone, Default, Debug, PartialEq)]
+	struct Velocity {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
+
 	fn insert_entities(count: usize) -> World {
 		let mut world = World::new();
 		for _ in 0..count {
-			let mut entity = world.spawn();
+			let mut entity = world.spawn_empty();
 			entity.insert(Position::default());
 			entity.insert(Rotation::default());
 			entity.insert(Velocity { x: 1.0, y: 1.0, z: 1.0 });
@@ -95,6 +95,27 @@ mod hecs_bench {
 	use super::*;
 	use hecs::*;
 
+	#[derive(Copy, Clone, Default, Debug, PartialEq)]
+	struct Position {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
+
+	#[derive(Copy, Clone, Default, Debug, PartialEq)]
+	struct Rotation {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
+
+	#[derive(Copy, Clone, Default, Debug, PartialEq)]
+	struct Velocity {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
+
 	fn insert_entities(count: usize) -> World {
 		let mut world = World::new();
 		for _ in 0..count {
@@ -155,9 +176,116 @@ mod hecs_bench {
 	}
 }
 
+mod flax_bench {
+	use super::*;
+	use flax::*;
+
+	#[derive(Copy, Clone, Default, Debug, PartialEq)]
+	struct Vec3 {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
+	
+	component! {
+		pos: Vec3,
+		rot: Vec3,
+		vel: Vec3,
+	}
+
+	fn insert_entities(count: usize) -> World {
+		let mut world = World::new();
+		for _ in 0..count {
+			EntityBuilder::new()
+				.set_default(pos())
+				.set_default(rot())
+				.set(vel(), Vec3 { x: 1.0, y: 1.0, z: 1.0})
+				.spawn(&mut world);
+		}
+		world
+	}
+
+	pub struct SimpleInsert;
+
+	impl SimpleInsert {
+		pub fn new() -> Self {
+			Self
+		}
+	
+		pub fn run(&mut self) {
+			insert_entities(ITER_COUNT);
+		}
+
+		pub fn run_batched(&mut self) {
+			let mut world = World::new();
+			let mut batch = BatchSpawn::new(ITER_COUNT);
+			
+			batch
+				.set(pos(), (0..).map(|_| { Vec3::default() }))
+				.expect("Invalid length");
+
+			batch
+				.set(rot(), (0..).map(|_| { Vec3::default() }))
+				.expect("Invalid length");
+
+			batch
+				.set(vel(), (0..).map(|_| { Vec3 { x: 1.0, y: 1.0, z: 1.0} }))
+				.expect("Invalid length");
+				
+			batch.spawn(&mut world);
+
+		}
+	}	
+
+	pub struct SimpleIter {
+		world: World,
+		query: Query<(Mutable<Vec3>, Component<Vec3>)>,
+	}
+
+	impl SimpleIter {
+		pub fn new() -> Self {
+			let world = insert_entities(ITER_COUNT);
+
+			// with flax we can cache the query easily 
+			let query = Query::new((pos().as_mut(), vel()));
+			Self {
+				world,
+				query
+			}
+		}
+	
+		pub fn run(&mut self) {
+			for (pos, vel) in &mut self.query.borrow(&self.world) {
+				*pos = Vec3 { x: pos.x + vel.x, y: pos.y + vel.y, z: pos.z + vel.z };
+			}
+		}
+	}
+}
+
 mod flecs_bench {
 	use super::*;
 	use flecs::*;
+
+	#[derive(Copy, Clone, Default, Debug, PartialEq)]
+	struct Position {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
+
+	#[derive(Copy, Clone, Default, Debug, PartialEq)]
+	struct Rotation {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
+
+	#[derive(Copy, Clone, Default, Debug, PartialEq)]
+	struct Velocity {
+		x: f32,
+		y: f32,
+		z: f32,
+	}
 
 	fn insert_entities(count: usize) -> World {
 		let mut world = World::new();
@@ -245,6 +373,14 @@ fn bench_simple_insert(c: &mut Criterion) {
         let mut bench = hecs_bench::SimpleInsert::new();
         b.iter(move || bench.run_batched());
     });
+    group.bench_function("flax_single", |b| {
+        let mut bench = flax_bench::SimpleInsert::new();
+        b.iter(move || bench.run());
+    });
+    group.bench_function("flax_batched", |b| {
+        let mut bench = flax_bench::SimpleInsert::new();
+        b.iter(move || bench.run_batched());
+    });
     group.bench_function("flecs", |b| {
         let mut bench = flecs_bench::SimpleInsert::new();
         b.iter(move || bench.run());
@@ -259,6 +395,10 @@ fn bench_simple_iter(c: &mut Criterion) {
     });
     group.bench_function("hecs", |b| {
         let mut bench = hecs_bench::SimpleIter::new();
+        b.iter(move || bench.run());
+    });
+    group.bench_function("flax", |b| {
+        let mut bench = flax_bench::SimpleIter::new();
         b.iter(move || bench.run());
     });
     group.bench_function("flecs_each", |b| {
