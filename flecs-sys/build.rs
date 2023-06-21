028441bfe4336ce0bee3b8f7554c0d3f921a812d
Mossa <cgmossa@gmail.com>
2023-06-21 09:33:16 +0200

    `cargo fmt`

--- flecs-sys/build.rs
+++ flecs-sys/build.rs
@@ -36,73 +36,69 @@ fn main() {
 		let include_flag = String::from("-I") + &include_path[..include_path.len()];
 		println!("Used Include Path: {}", include_path);
 
-        bindings = bindings.clang_arg(include_flag);
+		bindings = bindings.clang_arg(include_flag);
 	}
 
-	let bindings = bindings.generate().expect("Unable to generate bindings");
+	// export comments from flecs source
+	let bindings = bindings
+		.generate_comments(true)
+		.clang_arg("-fparse-all-comments")
+		// this yields two small comments
+		.clang_arg("-fretain-comments-from-system-headers")
+		.parse_callbacks(Box::new(CommentsCallbacks));
 
-    // export comments from flecs source
-    let bindings = bindings
-        .generate_comments(true)
-        .clang_arg("-fparse-all-comments")
-        // this yields two small comments
-        .clang_arg("-fretain-comments-from-system-headers")
-        .parse_callbacks(Box::new(CommentsCallbacks));
+	let bindings = bindings
+		.allowlist_file("flecs.c")
+		.allowlist_file("flecs.h")
+		.generate()
+		.expect("Unable to generate bindings");
 
-    let bindings = bindings
-        .allowlist_file("flecs.c")
-        .allowlist_file("flecs.h")
-        .generate()
-        .expect("Unable to generate bindings");
+	// We generate bindings to an actual source file so that we get better IDE integration
+	// Sadly to publish on crates.io we cannot write outside the OUT_DIR revisit this later.
+	// let out_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+	// bindings
+	// 	.write_to_file(out_path.join("src/bindings.rs"))
+	// 	.expect("Couldn't write bindings!");
 
-    // We generate bindings to an actual source file so that we get better IDE integration
-    // Sadly to publish on crates.io we cannot write outside the OUT_DIR revisit this later.
-    // let out_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    // bindings
-    // 	.write_to_file(out_path.join("src/bindings.rs"))
-    // 	.expect("Couldn't write bindings!");
+	let bindings_generated = bindings.to_string();
+	if cfg!(feature = "export_bindings") {
+		let out_path: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
+		std::fs::write(out_path.join("bindings.rs"), bindings_generated.clone())
+			.expect("Couldn't write bindings!");
+	}
 
-    let bindings_generated = bindings.to_string();
-    if cfg!(feature = "export_bindings") {
-        let out_path: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
-        std::fs::write(out_path.join("bindings.rs"), bindings_generated.clone())
-            .expect("Couldn't write bindings!");
-    }
+	let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+	std::fs::write(out_path.join("bindings.rs"), bindings_generated)
+		.expect("Couldn't write bindings!");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    std::fs::write(out_path.join("bindings.rs"), bindings_generated)
-        .expect("Couldn't write bindings!");
-
-    // Compile flecs C right into our Rust crate
-    cc::Build::new()
-        .warnings(true)
-        .extra_warnings(true)
-        .define("NDEBUG", None)
-        // .flag("-flto")			// no impact on Arm. Perhaps useful to other archs.
-        // .flag("-fuse-ld=lld")	// not available on MacOS/Arm
-        .file("flecs.c")
-        .compile("flecs");
+	// Compile flecs C right into our Rust crate
+	cc::Build::new()
+		.warnings(true)
+		.extra_warnings(true)
+		.define("NDEBUG", None)
+		// .flag("-flto")			// no impact on Arm. Perhaps useful to other archs.
+		// .flag("-fuse-ld=lld")	// not available on MacOS/Arm
+		.file("flecs.c")
+		.compile("flecs");
 }
 
 #[derive(Debug)]
 struct CommentsCallbacks;
 
 impl bindgen::callbacks::ParseCallbacks for CommentsCallbacks {
-    fn process_comment(&self, comment: &str) -> Option<String> {
-        // 1: trimming the comments
-        let comment = comment.trim();
-        // 2: brackets do not entail intra-links
-        let comment = comment.replace("[", "\\[");
-        let comment = comment.replace("]", "\\]");
+	fn process_comment(&self, comment: &str) -> Option<String> {
+		// 1: trimming the comments
+		let comment = comment.trim();
+		// 2: brackets do not entail intra-links
+		let comment = comment.replace("[", "\\[");
+		let comment = comment.replace("]", "\\]");
 
-        // ensure all links are padded with < and >
-        let url_re = regex::Regex::new(r"(?P<url>https?://[^\s]+)").unwrap();
-        let comment = url_re
-            .replace_all(comment.as_str(), |caps: &regex::Captures| {
-                format!("<{}>", &caps["url"])
-            })
-            .into_owned();
+		// ensure all links are padded with < and >
+		let url_re = regex::Regex::new(r"(?P<url>https?://[^\s]+)").unwrap();
+		let comment = url_re
+			.replace_all(comment.as_str(), |caps: &regex::Captures| format!("<{}>", &caps["url"]))
+			.into_owned();
 
-        Some(comment)
-    }
+		Some(comment)
+	}
 }
